6138b7439be651c5e8ea155ebad12d2dfa23c2ff
kotenkoI <iryna.herashchenko@gmail.com>
2024-06-23 13:48:01 +0300

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/es/integration/react";
+import App from "./components/App.jsx";
+import { store, persistor } from "./redux/store.js";
+import "modern-normalize";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
